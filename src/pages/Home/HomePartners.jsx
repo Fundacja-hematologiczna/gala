bccfd8b7750f922d081cf378a699a9e5aa7a7488
Piotr Matejek <devmatejekpior@gmail.com>
2025-06-06 10:17:30 +0200

    refactor HomePartners

--- src/pages/Home/HomePartners.jsx
+++ src/pages/Home/HomePartners.jsx
@@ -1,9 +1,34 @@
 import { Button } from '../../components/Button/Button';
 import { Slider } from '../../components/Slider/Slider';
 import { useTranslation } from 'react-i18next';
+import { usePartnerCategories } from '../../hooks/usePartnerCategories';
 
 const HomePartners = () => {
   const { t } = useTranslation();
+  const { honor, media, patron, sponsor } = usePartnerCategories();
+
+  const patrons = [
+    {
+      id: 'honor',
+      title: t('MAIN_PAGE.HONORARY_PATRONS'),
+      slides: honor,
+    },
+    {
+      id: 'media',
+      title: t('MAIN_PAGE.MEDIA_PATRONS'),
+      slides: media,
+    },
+    {
+      id: 'patron',
+      title: t('MAIN_PAGE.PATRONS'),
+      slides: patron,
+    },
+    {
+      id: 'sponsor',
+      title: t('MAIN_PAGE.PARTNERS'),
+      slides: sponsor,
+    },
+  ];
 
   return (
     <section className="Home__partners">
@@ -25,7 +50,7 @@ const HomePartners = () => {
       </div>
       <h2 className="Home__partners-title">{t('MAIN_PAGE.DONORS_PARTNERS')}</h2>
       <div className="Home__partners-slider">
-        <Slider />
+        <Slider patrons={patrons} />
       </div>
     </section>
   );
