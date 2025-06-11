36fb0f73cbbc476601fe0c960d3b34a982111a9d
Piotr Matejek <devmatejekpior@gmail.com>
2025-06-11 06:19:26 +0200

    added filter for logo category as sponsor

--- src/pages/Contributors/Contributors.jsx
+++ src/pages/Contributors/Contributors.jsx
@@ -14,7 +14,7 @@ const Contributors = () => {
       .then((response) => {
         if (response.data.data && Array.isArray(response.data.data)) {
           const logosForGala = response.data.data.filter(
-            (logo) => logo.on_gala === true && logo.gala_category,
+            (logo) => logo.on_gala === true && logo.gala_category === 'sponsor',
           );
           setLogos(logosForGala);
         }
